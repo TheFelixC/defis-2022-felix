3d808a641d0a8eb24900651faed8b8aceebb07d7
Felix Campeau <4000329@keranna.qc.ca>
2022-11-09 18:01:37 -0500

    Gyro &  Encoder

--- robot.py
+++ robot.py
@@ -7,6 +7,14 @@ class Robot(wpilib.TimedRobot):
         self.motor_gauche = wpilib.VictorSP(0)
         self.motor_droit = wpilib.VictorSP(1)
         self.drive = wpilib.drive.DifferentialDrive(self.motor_gauche, self.motor_droit)
+        self.encoder = wpilib.Encoder(0, 1)
+        self.gyro = wpilib.AnalogGyro(1)
+
+    def autonomousPeriodic(self):
+        if self.gyro.getAngle() <= 89:
+            self.drive.arcadeDrive(0, 1)
+        else:
+            self.drive.arcadeDrive(0, 0)
 
     def teleopPeriodic(self):
         self.drive.arcadeDrive(-1 * self.joystick.getY(), self.joystick.getX())
